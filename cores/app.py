6107579f47e9dc4c13a1ba1d7e4b14f075ce9ba0
zhangming <zhangming@ec3s.com>
2014-05-09 10:37:06 +0800

    add reponse and request

--- cores/app.py
+++ cores/app.py
@@ -1,11 +1,25 @@
 # coding: utf-8
 
 from bottle import Bottle, run as _run
+from bottle import LocalResponse as _LocalResponse
+from bottle import LocalRequest as _LocalRequest
+
 
 class App(Bottle):
     pass
 
+
+class LocalRequest(_LocalRequest):
+    pass
+
+
+class LocalResponse(_LocalResponse):
+    pass
+
+
 app = App()
+response = LocalResponse()
+request = LocalRequest()
 
 
 def run(host='127.0.0.1', port='5002', debug=False):
