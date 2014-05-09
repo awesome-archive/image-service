4c27763979595a69c722894fed4b5b13c7d8f9cf
baocaixiong <baocaixiong@gmail.com>
2014-05-09 12:05:13 +0800

    x

--- cores/handlers.py
+++ cores/handlers.py
@@ -5,6 +5,4 @@ from .constants import Constants
 
 @app.get(r'/captcha/token')
 def token():
-    app.captcha_coll.insert({'nihao': 'buhao'})
-    count = app.captcha_coll.count()
     return str(count)
