ac4b67a424988e1ed7515b73353885ab64f1cfeb
zhangming <baocaixiong@gmail.com>
2014-05-15 21:58:04 +0800

    调整图片handler策略，档图片库里没有图片的时候，直接同步生成

--- cores/http_/handlers.py
+++ cores/http_/handlers.py
@@ -3,9 +3,11 @@
 from .app import app, response, request, HTTPResponse
 from ..constants import Constants
 from cores.utils import generat_random_captcha
+
 from random import random
 from base64 import b64decode
-
+from copy import deepcopy
+from time import time
 
 @app.get(r'/captcha/<token>')
 def captcha_token(token=None):
@@ -23,10 +25,21 @@ def captcha_token(token=None):
 def captcha():
     coll = app.captcha_coll
     rand = random()
-    expires = request.params.get('expires')
+
+    _expires = request.params.get('expires', '3600')
+    expires = float(_expires) if _expires.isdigit() else 3600
+
     content = coll.find_one({'random': {"$gte": rand}, 'used': False}, expires=expires)
 
-    if content:
-        return content
-    else:
-        return generat_random_captcha().to_dict()
+    if not content:
+        image_dict = generat_random_captcha().to_dict()
+        content = deepcopy(image_dict)
+
+        image_dict['used'] = True
+        image_dict['expires'] = time() + expires
+        coll.insert(image_dict)
+
+    if '_id' in content:
+        del content
+
+    return content
